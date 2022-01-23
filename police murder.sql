c06397c4985e49b682aff2a3c342fb9c5a48ceec
Sakey_Isu <30744769+thealmanac1@users.noreply.github.com>
2022-01-23 10:38:49 -0500

    Update police murder.sql

--- police murder.sql
+++ police murder.sql
@@ -20,10 +20,12 @@ FROM [Police Voilence]..['2013-2021 Killings by State$']
 WHERE [# Black people killed] IS NOT NULL 
 ORDER BY [# Black people killed] desc
 
-SELECT KBS.State,PB.[Victim's race],PB.[Armed/Unarmed Status]
+SELECT KBS.State,PB.[Victim's race],PB.[Armed/Unarmed Status],PB.[Link to news article or photo of official document]
 FROM [Police Voilence]..['2013-2021 Killings by State$'] KBS
 JOIN [Police Voilence]..['2013-2021 Police Killings$'] PB
 	ON PB.State = KBS.[State Abbreviation]
+	WHERE [Victim's race]='Black'
+	Order By State
 
 CREATE VIEW armed_info AS
 SELECT KBS.State,PB.[Victim's race],PB.[Armed/Unarmed Status],PB.[Link to news article or photo of official document],
@@ -38,9 +40,41 @@ WHERE [Victim's race]='Black' and [Armed/Unarmed Status]='Unarmed/Did Not Have A
 GROUP BY [Victim's race],[Armed/Unarmed Status],State
 ORDER BY total_unarmed_black_people_killed desc;
 
+SELECT [Victim's race],[Armed/Unarmed Status],State
+FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE [Victim's race]='Black' and [Armed/Unarmed Status]='Unarmed/Did Not Have Actual Weapon' and State = State
 
+SELECT State,[Total Population of Census Tract 2019 ACS 5-Year Estimates],COUNT(*) AS people_killed
+FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE State = State
+GROUP BY State
+ORDER BY people_killed desc;
 
+SELECT PD,[All People Killed by Police (1/1/2013-12/31/2021)],[Black People Killed by Police (1/1/2013-12/31/2021)],[White People Killed by Police (1/1/2013-12/31/2021)],[Asian People Killed by Police (1/1/2013-12/31/2021)]
+FROM [Police Voilence]..['2013-2021 Killings by PD$']
+WHERE State is not null and ORI is not null
+ORDER BY [Asian People Killed by Police (1/1/2013-12/31/2021)] desc
 	
+SELECT State,[Date of Incident (month/day/year)],[Victim's age],[Victim's race],[Victim's gender],[Link to news article or photo of official document],[Cause of death],[Official disposition of death (justified or other)],[Armed/Unarmed Status] FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE [Victim's name] is not null and [Victim's race]= 'Black' and [Armed/Unarmed Status]= 'Unarmed/Did Not Have Actual Weapon' 
+ORDER BY [Victim's age] asc
+
+SELECT State,[Victim's name],[Victim's age],[Victim's race],[Victim's gender],[Link to news article or photo of official document],[Cause of death],[Official disposition of death (justified or other)],[Armed/Unarmed Status] FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE [Victim's name] is not null and [Victim's race]= 'Black'
+ORDER BY [Victim's age] asc
+
+SELECT State,[Date of Incident (month/day/year)],[Victim's age],[Victim's race],[Victim's gender],[Link to news article or photo of official document],[Cause of death],[Official disposition of death (justified or other)],[Armed/Unarmed Status],[Killed by Police 2013-21]
+FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE [Victim's name] is not null and [Victim's race]= 'Black'
+ORDER BY [Victim's age] asc
+
+SELECT [Date of Incident (month/day/year)],State,[Victim's name],[Victim's age],[Victim's race],[Victim's gender],[Link to news article or photo of official document],[Cause of death],[Official disposition of death (justified or other)],[Armed/Unarmed Status] FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE [Victim's name] is not null and [Victim's race]= 'Black' and [Cause of death]='Beaten'
+ORDER BY [Victim's age] asc
+
+SELECT [Date of Incident (month/day/year)],State,[Victim's name],[Victim's age],[Victim's race],[Victim's gender],[Link to news article or photo of official document],[Cause of death],[Official disposition of death (justified or other)],[Armed/Unarmed Status] FROM [Police Voilence]..['2013-2021 Police Killings$']
+WHERE [Victim's name] is not null and [Cause of death]='Beaten'
+ORDER BY [Victim's age] asc
 
 	
 
